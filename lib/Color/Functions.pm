ae72d2456b91d0b8025c6f88d8cbdf1b127cce36
Darren Embry <dse@webonastick.com>
2023-01-24 18:27:49 -0500

    blah

--- lib/Color/Functions.pm
+++ lib/Color/Functions.pm
@@ -3,6 +3,31 @@ use warnings;
 use strict;
 use List::Util qw(min max);
 
+use base "Exporter";
+
+our @EXPORT = qw();
+our @EXPORT_OK = (
+    'clamp',
+    'srgb_to_linear',
+    'linear_to_srgb',
+    'linear_color_mix',
+    'linear_luminance_srgb',
+    'linear_luminance_709',
+    'linear_luminance_601',
+    'linear_luminance_2020',
+    'linear_luminance_240',
+    'linear_hsv',
+    'linear_hsl',
+    'linear_hue_hsv_hsl',
+    'linear_value_hsv',
+    'linear_saturation_hsv',
+    'linear_saturation_hsl',
+    'linear_lightness_hsl',
+);
+our %EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+
 our $GAMMA_2020    = 2.2;
 our $ALPHA_2020    = 1.099;           # 1.099296826809442
 our $A_2020        = $ALPHA_2020 - 1; # 0.099
@@ -21,7 +46,7 @@ our $GAMMA_SRGB    = 2.4;
 our $ALPHA_SRGB    = 1.055;
 our $A_SRGB        = $ALPHA_SRGB - 1; # 0.055
 our $BETA_SRGB     = 0.0031308;
-our $THINGY_SRGB   = 12.92;                     # orig 12.9232102
+our $THINGY_SRGB   = 12.92;     # orig 12.9232102
 our $THINGY_2_SRGB = $THINGY_SRGB * $BETA_SRGB; # 0.04045 orig 0.0392857 from obsolete early draft
 
 sub clamp {
@@ -73,6 +98,11 @@ sub mix {
 # relative luminance is the weighted sum of linear RGB components
 
 # ITU BT.709 --- HDTV
+
+sub linear_luminance_srgb {
+    goto &linear_luminance_709;
+}
+
 sub linear_luminance_709 {
     @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
     my ($r, $g, $b) = map { clamp($_) } @_;
@@ -103,7 +133,28 @@ sub linear_luminance_240 {
     return 0.212 * $r + 0.701 * $g + 0.087 * $b;
 }
 
-sub linear_hue {
+sub linear_hsv {
+    @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
+    my ($r, $g, $b) = map { clamp($_) } @_;
+    my $h = linear_hue_hsv_hsl($r, $g, $b);
+    my $s = linear_saturation_hsv($r, $g, $b);
+    my $v = linear_value_hsv($r, $g, $b);
+    return ($h, $s, $v) if wantarray;
+    return [$h, $s, $v];
+}
+
+sub linear_hsl {
+    @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
+    my ($r, $g, $b) = map { clamp($_) } @_;
+    my $h = linear_hue_hsv_hsl($r, $g, $b);
+    my $s = linear_saturation_hsl($r, $g, $b);
+    my $l = linear_lightness_hsl($r, $g, $b);
+    return ($h, $s, $l) if wantarray;
+    return [$h, $s, $l];
+}
+
+# HSV and HSL
+sub linear_hue_hsv_hsl {
     @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
     my ($r, $g, $b) = map { clamp($_) } @_;
 
@@ -132,7 +183,13 @@ sub linear_saturation_hsv {
     @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
     my ($r, $g, $b) = map { clamp($_) } @_;
 
-    # eh?
+    my $max = max($r, $g, $b);
+    my $min = min($r, $g, $b);
+    my $c = $max - $min;
+    if (near_equal($max, 0)) {
+        return 0;
+    }
+    return $c / $max;
 }
 
 sub linear_saturation_hsl {
@@ -142,68 +199,60 @@ sub linear_saturation_hsl {
     my $max = max($r, $g, $b);
     my $min = min($r, $g, $b);
     my $c = $max - $min;        # range
+    my $l = ($max + $min) / 2;
     if (near_equal($c, 0)) {
         return 0;               # meaningless
     }
-    my $s;
-    my $l = ($max + $min) / 2.0;
-    my $delta = $max - $min;
-    if ($l < 0.5) {
-        $s = $delta / ($max + $min);
-    } else {
-        $s = $delta / (2.0 - $max - $min);
-    }
-    return $s;
+    return $c / (1 - abs(2 * $l - 1));
 }
 
-# HSL
-sub linear_lightness {
-    @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
-    my ($r, $g, $b) = map { clamp($_) } @_;
-    return (min($r, $g, $b) + max($r, $g, $b)) / 2;
+sub linear_brightness_hsv {
+    goto &linear_value_hsv;
 }
 
-# HSV
-sub linear_value {
-    goto &linear_brightness;
-}
-sub linear_brightness {
+sub linear_value_hsv {
     @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
     my ($r, $g, $b) = map { clamp($_) } @_;
     return max($r, $g, $b);
 }
 
-sub linear_chrominance {
+sub linear_lightness_hsl {
     @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
     my ($r, $g, $b) = map { clamp($_) } @_;
-    # eh?
+    return ($r + $g + $b) / 3;
 }
+
+# sub linear_chrominance {
+#     @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
+#     my ($r, $g, $b) = map { clamp($_) } @_;
+#     # eh?
+# }
 
 # https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
-sub luminance_to_lightness {
-    @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
-    my ($y) = map { clamp($_) } @_;
+# sub linear_luminance_to_lightness {
+#     @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
+#     my ($y) = map { clamp($_) } @_;
 
-    if ($y <= 0.00856) {        # 216/24389
-        return $y * 903.3;      # 24389/27
-    } else {
-        return $y ** (1/3) * 116 - 16;
-    }
-    # L* = 50 when Y = 18.4 or 18% gray card
-}
+#     if ($y <= 0.00856) {        # 216/24389
+#         return $y * 903.3;      # 24389/27
+#     } else {
+#         return $y ** (1/3) * 116 - 16;
+#     }
+#     # L* = 50 when Y = 18.4 or 18% gray card
+# }
 
-sub contrast_ratio {
-    @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
-    my ($r1, $g1, $b1, $r2, $g2, $b2) = map { clamp($_) } @_;
-    my $y1 = linear_luminance_srgb($r1, $g1, $b1);
-    my $y2 = linear_luminance_srgb($r2, $g2, $b2);
-    if ($y1 < $y2) {
-        ($y1, $y2) = ($y2, $y1);
-    }
-    return ($y1 + 0.05) / ($y2 + 0.05);
-    # 1 = no contrast
-    # 21 = max contrast
-}
+# sub linear_contrast_ratio {
+#     @_ = map { (ref $_ eq 'ARRAY') ? @$_ : ($_) } @_;
+#     my ($r1, $g1, $b1, $r2, $g2, $b2) = map { clamp($_) } @_;
+#     my $y1 = linear_luminance_srgb($r1, $g1, $b1);
+#     my $y2 = linear_luminance_srgb($r2, $g2, $b2);
+#     if ($y1 < $y2) {
+#         ($y1, $y2) = ($y2, $y1);
+#     }
+#     return ($y1 + 0.05) / ($y2 + 0.05);
+#     # 1 = no contrast
+#     # 21 = max contrast
+# }
 
 sub near_equal {
     my ($a, $b) = @_;
